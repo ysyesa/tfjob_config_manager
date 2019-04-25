703cbc2b3ea49b083e19df75c04fe362798334cb
Yesa <yesasurya01@gmail.com>
2019-04-26 00:26:13 +0700

    Init project

---
+++ ConfigManager.py
@@ -0,0 +1,123 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import subprocess
+
+app = Flask(__name__)
+
+
+def load_template():
+    template = []
+    with open("template.yaml", "r") as fi:
+        line = fi.readline()
+        while line:
+            template.append(line)
+            line = fi.readline()
+        fi.close()
+    return template
+
+
+def write_template(template):
+    with open("output.yaml", "w") as fi:
+        for i in range(len(template)):
+            fi.write(template[i])
+        fi.close()
+
+
+TEMPLATE = load_template()
+
+
+@app.route("/", methods=["GET", "POST"])
+def root():
+    return jsonify("The ConfigurationListener is working.")
+
+
+@app.route("/modify", methods=["POST"])
+def modify():
+    tfjob_meta_name = request.form["tfjob_meta_name"]
+    tfjob_master_replica = request.form["tfjob_master_replica"]
+    tfjob_master_image = request.form["tfjob_master_image"]
+    tfjob_worker_replica = request.form["tfjob_worker_replica"]
+    tfjob_worker_image = request.form["tfjob_worker_image"]
+    tfjob_ps_replica = request.form["tfjob_ps_replica"]
+    tfjob_ps_image = request.form["tfjob_ps_image"]
+    tfjob_path_dataset = request.form["tfjob_path_dataset"]
+    tfjob_path_tensorboard = request.form["tfjob_path_tensorboard"]
+    tfjob_total_epoch = int(request.form["tfjob_total_epoch"])
+    tfjob_next_epoch = int(request.form["tfjob_next_epoch"])
+
+    assert tfjob_meta_name is not None
+    assert tfjob_master_replica is not None
+    assert tfjob_master_image is not None
+    assert tfjob_worker_replica is not None
+    assert tfjob_worker_image is not None
+    assert tfjob_ps_replica is not None
+    assert tfjob_ps_image is not None
+    assert tfjob_path_dataset is not None
+    assert tfjob_path_tensorboard is not None
+    assert tfjob_total_epoch is not None
+    assert tfjob_next_epoch is not None
+
+    if tfjob_next_epoch > tfjob_total_epoch:
+        message = "Final epoch (#" + str(tfjob_total_epoch) + ") has reached. Training is done."
+        print message
+        return jsonify(message)
+    else:
+
+        c = ConfigManager(tfjob_meta_name, TEMPLATE)
+        c.set_master_replica(tfjob_master_replica)
+        c.set_master_image(tfjob_master_image)
+        c.set_worker_replica(tfjob_worker_replica)
+        c.set_worker_image(tfjob_worker_image)
+        c.set_ps_replica(tfjob_ps_replica)
+        c.set_ps_image(tfjob_ps_image)
+        c.set_path_dataset(tfjob_path_dataset)
+        c.set_path_tensorboard(tfjob_path_tensorboard)
+
+        write_template(c.template)
+
+        subprocess.call(["kubectl", "delete", "tfjob", tfjob_meta_name])
+        subprocess.call(["kubectl", "apply", "-f", "output.yaml"])
+
+        message = "Configuration generated and applied for epoch #" + str(tfjob_next_epoch)
+        print message
+        return jsonify(message)
+
+
+class ConfigManager:
+    def __init__(self, meta_name, template):
+        self.meta_name = meta_name
+        self.template = template
+        self.edit_template_value(3, self.meta_name)
+
+    def edit_template_value(self, index, value):
+        strings = self.template[index].split(":")
+        self.template[index] = strings[0] + ": " + value
+
+    def set_master_replica(self, number):
+        self.edit_template_value(7, number)
+
+    def set_master_image(self, image):
+        self.edit_template_value(12, image)
+
+    def set_worker_replica(self, number):
+        self.edit_template_value(31, number)
+
+    def set_worker_image(self, image):
+        self.edit_template_value(36, image)
+
+    def set_ps_replica(self, number):
+        self.edit_template_value(55, number)
+
+    def set_ps_image(self, image):
+        self.edit_template_value(60, image)
+
+    def set_path_dataset(self, path):
+        self.edit_template_value(26, path)
+        self.edit_template_value(50, path)
+        self.edit_template_value(74, path)
+
+    def set_path_tensorboard(self, path):
+        self.edit_template_value(29, path)
+        self.edit_template_value(53, path)
+        self.edit_template_value(77, path)
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+FLASK_APP=ConfigListener.py flask run --host=0.0.0.0 &
